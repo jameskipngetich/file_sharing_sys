f2c7023d28cd79c3bd89143c46f87c712b1193b9
James Kipngetich <jameskipngetich166@gmail.com>
2026-02-08 23:20:26 +0300

    Fix some syntactical bugs

--- file_upload.py
+++ file_upload.py
@@ -13,7 +13,7 @@ s3_client = boto3.client('s3')
 dynamo_db = boto3.resource('dynamodb')
 table = dynamo_db.Table('secure-fle-db')        # TODO: create a dynamodb table named so
 BUCKET_NAME = "mybucket"                        # TODO: enter the name of the s3 bucket to be used
-PRESIGNED_URL_EXPIRY = "300"                    # 5 minutes
+PRESIGNED_URL_EXPIRY = 300                    # 5 minutes
 
 
 def lambda_handler(event, context):
@@ -29,7 +29,7 @@ def lambda_handler(event, context):
         content_type = body['content-type']
 
         # Get the user_id
-        user_id = body.requestContext.authorizer.claims['sub']
+        user_id = event['requestContext']['authorizer']['claims']['sub']
 
         # Generate unique file_id and s3 key (user_id/file_id-filename)
         file_id = str(uuid.uuid4())
@@ -40,27 +40,30 @@ def lambda_handler(event, context):
             'put_object',
             {
                 "Bucket": BUCKET_NAME,
-                "key": s3_key,
-                "content-type": content_type
+                "Key": s3_key,
+                "ContentType": content_type
             },
             PRESIGNED_URL_EXPIRY
         )
 
         # Save metadata to dynamodb
-        dynamo_db.put_item(file_id=file_id)
-        dynamo_db.put_item(user_id=user_id)
-        dynamo_db.put_item(filename=filename)
-        dynamo_db.put_item(s3_key=s3_key)
-        dynamo_db.put_item(content_type=content_type)
-        dynamo_db.put_item(upload_date=datetime.now())
-        dynamo_db.put_item(status="PENDING")
+        table.put_item(Item= {
+            'file_id': file_id,
+            'user_id': user_id,
+            'filename': filename,
+            's3_key': s3_key,
+            'content_type': content_type,
+            'upload_date': datetime.now().isoformat(),
+            'status': "PENDING"
+            }
+            )
 
         return{
             'statusCode': 200,
-            'body': {
+            'body': json.dumps({
                 'url': f'{url}',
                 'file_id': f'{file_id}'
-            }
+            })
         }
 
 
