aaab130628b71a7299c84870e828af6af7c4f63b
James Kipngetich <jameskipngetich166@gmail.com>
2026-02-23 10:43:39 +0300

    Feat: Implemented IaC

--- main.tf
+++ main.tf
@@ -1,14 +1,137 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 # TODO ADDD IaC logic for the aws services used in the application
+
+# TODO add network IaC -> vpc-> public subnets, and private subnets(db) -> security groups for lambda
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  cidr   = "10.0.0.0/16"
+  name   = var.vpc_name
+  tags = {
+    name        = var.vpc_name
+    environment = "dev"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = vpc.vpc_id
+  cidr_block = "10.0.1.0/16"
+
+  tags = {
+    name = "public_subnet"
+  }
+  # how do we attach internet gte waay
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = vpc.vpc_id
+  cidr_block = "10.0.2.0/16"
+
+  tags = {
+    name = "private_subnet"
+  }
+}
+
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = vpc.vpc_id
+  # configure inbound and outbound rules
+
+  tags = {
+    name = "lambda_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-ssh" {
+  security_group_id = aws_security_group.lambda_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = "22"
+  ip_protocol       = "ssh"
+  to_port           = "22"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_ports" {
+  security_group_id = aws_security_group.lambda_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# ToDO add s3 bucket IaC (bucket name is a variable)
+
+module "aws_s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = var.bucket_name # remember to output bucket name
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  force_destroy = true
+}
+
+# TODO add api gateway IaC
+#TODO add lambda IaC -> how to add already written lambda handlers
+/*
+resource "aws_lambda" "filesharing_lambda" {
+  # do reasearch on this module
+}
+*/
+# TODO add dynamo db IaC -> a table -> with the provided attributes
+
+module "dynamodb_table" {
+  source                      = "terraform-aws-modules/dynamodb-table/aws"
+  name                        = var.dynamodb_table_name
+  hash_key                    = "id"
+  deletion_protection_enabled = false
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    },
+    {
+      name = "file_id"
+      type = "S"
+    },
+    {
+      name = "user_id"
+      type = "S"
+    },
+    {
+      name = "file_name"
+      type = "S"
+    },
+    {
+      name = "s3_key"
+      type = "S"
+    },
+    {
+      name = "content_type"
+      type = "S"
+    },
+    {
+      name = "upload_date"
+      type = "N" # what is the type of date
+    },
+    {
+      name = "status"
+      type = "S"
+    },
+    {
+      name = "id"
+      type = "N"
+    },
+
+  ] # do research on this module
+}
--- variables.tf
+++ variables.tf
@@ -1,17 +1,35 @@
-variable "project_name"{
-    description = "Project name for resource definition"
-    type = string
-    default = "uploadfile"
+variable "project_name" {
+  description = "Project name for resource definition"
+  type        = string
+  default     = "uploadfile"
 }
 
 variable "aws_region" {
-    description = "Define the AWS region"
-    type = string
-    default = "us-east-1"
+  description = "Define the AWS region"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "lambda_runtime" {
-    description = "Lambda runtime version"
-    type = string
-    default = "python 3.11"
+  description = "Lambda runtime version"
+  type        = string
+  default     = "python 3.11"
+}
+
+variable "vpc_name" {
+  description = "name of the vpc"
+  type        = string
+  default     = "filesharing_vpc"
+}
+
+variable "bucket_name" {
+  description = "name of the bucket to store the files"
+  type        = string
+  default     = "123_filesharing_bucket_321" # should be globally unique
+}
+
+variable "dynamodb_table_name" {
+  description = "The table name of the table to store file metadate"
+  type        = string
+  default     = "secure_upload_db"
 }
